2dc2c24e52714f6819b388c4c8273fe9929623a3
Saily Hurtado <sailyhurtado@gmail.com>
2025-04-29 13:49:53 +0200

    Challenge: Create the language chips. Use the  `languages.js` file to pull in the array of languages to use, which contains the language name, background color, and text color.

--- assembly_endgame/src/App.jsx
+++ assembly_endgame/src/App.jsx
@@ -1,14 +1,33 @@
 
+import { useState } from "react"
+import { languages } from "./assets/languages"
+import { nanoid } from 'nanoid'
 import './App.css'
 import GameStatus from './components/GameStatus'
 import Header from './components/Header'
+import LanguagesChips from "./components/LanguagesChips"
 
-export default function AssemblyEndgame() {  
 
-  return (
-    <main>
-        <Header />
-        <GameStatus />
-    </main>
-  )
+export default function AssemblyEndgame() {      
+    
+    const languagesElements = languages.map(lang => (
+        <LanguagesChips
+            key={nanoid()}
+            style={{
+                backgroundColor: lang.backgroundColor,
+                color: lang.color,
+            }}
+            value={lang.name}
+        />
+    ))
+
+    return (
+        <main>
+            <Header />
+            <GameStatus />        
+            <section className="languages-chips">
+                { languagesElements }
+            </section>
+        </main>
+    )
 }
---
+++ assembly_endgame/src/components/LanguagesChips.jsx
@@ -0,0 +1,13 @@
+
+
+export default function LanguagesChips(props) {
+    
+
+    return (
+        <span            
+            style={props.style}>
+            {props.value}                    
+        </span>
+        
+    )
+}
